4977ea1ee09f92f6372207205b6bf81c9792b652
Renato ST <renatostv@gmail.com>
2020-07-17 08:41:17 -0300

    Criação, edição e exclusão de produto
    - Página de criação de anúncio de produto.
    
    - Rotas para edição e exclusão dos anúncios
    
    - Formatação de entrada de input para unidade monetária

---
+++ public/scripts.js
@@ -0,0 +1,25 @@
+Mask = {
+    apply(input, func){
+        setTimeout(function () {
+            input.value = Mask[func](input.value)
+        }, 1)
+    },
+    formatBRL(value){
+        value = value.replace(/\D/g, "")
+
+        return new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(value/100)
+
+    }
+}
+
+const formDelete = document.querySelector("#form-delete")
+    formDelete.addEventListener("submit", function(event){
+        const confirmation = confirm("Deseja Deletar?")
+        if(!confirmation){
+            event.preventDefault()
+        
+        }
+    })
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -16,5 +16,11 @@ module.exports = {
             format:`${day}/${month}/${year}`
         }
 
+    },
+    formatPrice(price){
+        return  new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(price/100)
     }
 }
