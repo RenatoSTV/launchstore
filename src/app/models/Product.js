48b266f591bc81749e6be22a57daa2e406ea6e52
Renato ST <renatostv@gmail.com>
2020-08-13 22:24:08 -0300

    Listagem e Search
    - Listagem de produtos na página inicial
    - Busca e listagem de produtos

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -1,7 +1,12 @@
 const db = require('../../config/db')
 
 module.exports = {
-    
+    all(){
+        return db.query(`
+            SELECT * FROM products
+            ORDER BY updated_at DESC
+        `)
+    },
     create(data){
 
         const query = `
@@ -70,5 +75,32 @@ module.exports = {
         return db.query(`
             SELECT * FROM files WHERE product_id = $1
         `, [id])
+    },
+    search(paramas) {
+        const { filter, category } = paramas
+        let query = "",
+            filterQuery = `WHERE`
+        
+            if(category) {
+                filterQuery = `${filterQuery}
+                products.category_id = ${category}
+                AND`
+            }
+
+            filterQuery = `
+                ${filterQuery}
+                products.name ilike '%${filter}%'
+                OR products.description ilike '%${filter}%'
+            `
+
+            query = `
+                SELECT products.*,
+                    categories.name AS category_name
+                FROM products
+                LEFT JOIN categories ON (categories.id = products.category_id)
+                ${filterQuery}
+            `
+
+            return db.query(query)
     }
 }
